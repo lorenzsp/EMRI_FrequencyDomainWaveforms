b9f5870e6a3f4650381ffcf7e08872b182615476
Lorenzo Speri <lsperi@saraswati.aei.mpg.de>
2023-06-01 10:49:09 +0200

    new runs correct

---
+++ MCMC_submit.sh
@@ -0,0 +1,31 @@
+# nohup python emri_pe.py -Tobs 4.0 -M 8019425.9054786945 -mu 685.1946995167503 -p0 12.115436197262476 -e0 0.39141517457483055 -dev 7 -eps 1e-5 -dt 10.0 -injectFD 0 -template td -nwalkers 128 -ntemps 1 -downsample 0 > out_td_td.out &
+# nohup python emri_pe.py -Tobs 4.0 -M 8019425.9054786945 -mu 685.1946995167503 -p0 12.115436197262476 -e0 0.39141517457483055 -dev 6 -eps 1e-5 -dt 10.0 -injectFD 1 -template fd -nwalkers 128 -ntemps 1 -downsample 0 > out_fd_fd.out &
+# nohup python emri_pe.py -Tobs 4.0 -M 8019425.9054786945 -mu 685.1946995167503 -p0 12.115436197262476 -e0 0.39141517457483055 -dev 5 -eps 1e-5 -dt 10.0 -injectFD 0 -template fd -nwalkers 128 -ntemps 1 -downsample 0 > out_fd_td.out &
+
+Tobs=4.0
+M=3670041.7362535275
+mu=292.0583167470244
+p0=13.709101864726545
+e0=0.5794130830706371
+eps=1e-5
+dt=10.0
+nwalkers=128
+ntemps=1
+downsample=0
+
+dev=0
+injectFD=0
+template="fd"
+nohup python emri_pe.py -Tobs $Tobs -M $M -mu $mu -p0 $p0 -e0 $e0 -dev $dev -eps $eps -dt $dt -injectFD $injectFD -template $template -nwalkers $nwalkers -ntemps $ntemps -downsample $downsample > out_0.out &
+
+dev=6
+injectFD=1
+template="fd"
+nohup python emri_pe.py -Tobs $Tobs -M $M -mu $mu -p0 $p0 -e0 $e0 -dev $dev -eps $eps -dt $dt -injectFD $injectFD -template $template -nwalkers $nwalkers -ntemps $ntemps -downsample $downsample > out_1.out &
+
+dev=7
+injectFD=0
+template="td"
+nohup python emri_pe.py -Tobs $Tobs -M $M -mu $mu -p0 $p0 -e0 $e0 -dev $dev -eps $eps -dt $dt -injectFD $injectFD -template $template -nwalkers $nwalkers -ntemps $ntemps -downsample $downsample > out_2.out &
+
+# 603401  2954440  2954439
